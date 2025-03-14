f3f1d0d43eb935caf2b6ddf60532566c86acd247
lam-oli <olivia.patricia.lam@gmail.com>
2025-03-13 22:37:42 -0700

    Created table in SQL file

---
+++ cpsc368_proj.sql
@@ -0,0 +1,23 @@
+CREATE TABLE mental_health_data (
+    UserID INT PRIMARY KEY,
+    self_employed INT,
+    family_history VARCHAR(20),
+    employer_benefits VARCHAR(50),
+    sought_treatment INT,
+    company_employee_total VARCHAR(20), 
+    state_name VARCHAR(50),
+    anonymity_protection VARCHAR(20),
+    is_tech_company INT,
+    country_name VARCHAR(30),
+    age INT,
+    gender VARCHAR(10),
+    mh_in_interview VARCHAR(10)
+    );
+
+
+    
+    
+    
+    
+    
+    
